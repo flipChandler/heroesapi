d9c4162c6d559ac4a2f3afaf233460085096e407
flipChandler <felipe.santos11@hotmail.com>
2021-04-03 11:52:06 -0300

    add outros parametros de configuração

--- src/main/java/com/digitalinnovationone/heroesapi/config/HeroesData.java
+++ src/main/java/com/digitalinnovationone/heroesapi/config/HeroesData.java
@@ -21,10 +21,13 @@ import com.amazonaws.services.dynamodbv2.document.Item;
 import com.amazonaws.services.dynamodbv2.document.PutItemOutcome;
 import java.util.Arrays;
 
+import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.REGION_DYNAMO;
+import static com.digitalinnovationone.heroesapi.constants.HeroesConstant.ENDPOINT_DYNAMO;
+
 public class HeroesData {
     public static void main(String[] args) throws Exception {
         AmazonDynamoDB client = AmazonDynamoDBClientBuilder.standard()
-                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration())
+                .withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration(ENDPOINT_DYNAMO, REGION_DYNAMO))
                 .build();
 
         DynamoDB dynamoDB = new DynamoDB(client);
